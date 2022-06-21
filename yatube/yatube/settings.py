fc0ec88dcdcb7177330f081338bb6bf1946a81b2
Aleksandr Kozhushkevich <kazhuha@gmail.com>
2022-06-21 21:07:05 +0300

    dotenv add. seckret_key moved to .env

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 
-from yatube.config import secret_key
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key
+SECRET_KEY = os.getenv('SECRET_KEY', default='supersecretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
